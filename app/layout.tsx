e77c2aa156e0f647740481d8b4b2f06d43d8d636
Lars Berbusmel <lars.berbusmel@gmail.com>
2026-05-11 12:39:32 +0200

    Update site title to Misemetrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Micemetrics",
+  title: "Misemetrics",
   description: "Kalkulering",
     icons: {
     icon: "/logo.png",
